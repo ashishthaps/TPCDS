21cca2744e112883f102316a66f08b1c376e6642
Ashish Thapliyal <ashishthaps@hotmail.com>
2017-04-11 21:47:34 -0700

    Update tpcdshadoop.sh

--- tpcdshadoop.sh
+++ tpcdshadoop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/hdinsight/tpcds-datagen-as-hive-query/ && cd tpcds-datagen-as-hive-query
 hdfs dfs -copyFromLocal resources /tmp
+byobu
 beeline -u "jdbc:hive2://`hostname -f`:10001/;transportMode=http" -n "" -p "" -i settings.hql -f TPCDSDataGen.hql -hiveconf SCALE=1000 -hiveconf PARTS=1000 -hiveconf LOCATION=/HiveTPCDS/ -hiveconf TPCHBIN=`grep -A 1 "fs.defaultFS" /etc/hadoop/conf/core-site.xml | grep -o "wasb[^<]*"`/tmp/resources  
 beeline -u "jdbc:hive2://`hostname -f`:10001/;transportMode=http" -n "" -p "" -i settings.hql -f ddl/createAllORCTables.hql -hiveconf ORCDBNAME=tpcds_orc -hiveconf SOURCE=tpcds
 beeline -u "jdbc:hive2://`hostname -f`:10001/;transportMode=http" -n "" -p "" -i settings.hql -f ddl/analyze.hql -hiveconf ORCDBNAME=tpcds_orc 
